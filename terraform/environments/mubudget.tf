f9c8ecf8edb53aa154720b108665cd3101886e94
Andrea SonnY <andreasonny83@gmail.com>
2018-04-26 16:25:33 +0100

    (Terraform) S3 bucket name changed

--- terraform/environments/mubudget.tf
+++ terraform/environments/mubudget.tf
@@ -1,6 +1,6 @@
 module "mubudget" {
   source = "../modules/"
 
-  static_web_page_bucket = "mubudget.com"
+  static_web_page_bucket = "www.mubudget.tk"
   static_web_page_user   = "mubudget-s3-upload"
 }
